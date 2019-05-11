0cb4a41473d6dd79593d1b759a4d10223e7d0c72
Peter Law <PeterJCLaw@gmail.com>
2019-05-11 21:32:12 +0100

    Declare support for more recent versions of Python
    I've run the tests on each of these Python versions, plus poked it
    a bit manually; all seemed to work.
    
    Note the omission of Python 3.3 -- that's sufficiently old that
    I don't have an environment for it.

--- setup.py
+++ setup.py
@@ -84,6 +84,10 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.0",
     "Programming Language :: Python :: 3.1",
     "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
     ]
 
 if build_cext:
